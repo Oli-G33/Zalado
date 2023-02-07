a25d7a6661e02642686c0c329853be746ce8d149
OliGarc333 <ngarcia333@gmail.com>
2023-02-07 20:12:42 +0100

    Added Item, Footer, Subscribe

--- client/src/components/Item.jsx
+++ client/src/components/Item.jsx
@@ -1,11 +1,11 @@
 import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { IconButton, Box, Typography, useTheme, Button } from '@mui/material';
-import AddIcon from '@mui/material/Add';
+import AddIcon from '@mui/icons-material/Add';
 import RemoveIcon from '@mui/icons-material/Remove';
 import { shades } from '../theme';
 import { addToCart } from '../state';
-import { useNavigate } from '<react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 const Item = ({ item, width }) => {
   const navigate = useNavigate();
--- client/src/scenes/home/ShoppingList.jsx
+++ client/src/scenes/home/ShoppingList.jsx
@@ -1,7 +1,96 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Box, Typography, Tab, Tabs, useMediaQuery } from '@mui/material';
+import Item from '../../components/Item';
+import { useDispatch, useSelector } from 'react-redux';
+import { setItems } from '../../state';
+import { convertLength } from '@mui/material/styles/cssUtils';
 
 const ShoppingList = () => {
-  return <div>ShoppingList</div>;
+  const dispatch = useDispatch();
+  const [value, setValue] = useState();
+  const items = useSelector(state => state.cart.items);
+  const isNonMobile = useMediaQuery('(min-width:600px)');
+  console.log('items:', items);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  async function getItems() {
+    const items = await fetch(
+      'http://localhost:1337/api/items?populate=image',
+      { method: 'GET' }
+    );
+    const itemsJson = await items.json();
+    dispatch(setItems(itemsJson.data));
+  }
+
+  useEffect(() => {
+    getItems();
+  }, []); // eslint-disable-line react-hook/exhaustive-deps
+
+  const topRatedItems = items.filter(
+    item => item.attributes.category === 'topRated'
+  );
+  const newArrivalsItems = items.filter(
+    item => item.attributes.category === 'newArrivals'
+  );
+  const bestSellersItems = items.filter(
+    item => item.attributes.category === 'bestSellers'
+  );
+
+  return (
+    <Box width="80%" margin="80px auto">
+      <Typography variant="h3" textAlign="center">
+        Our Featured <b>Products</b>
+      </Typography>
+      <Tabs
+        textColor="primary"
+        indicatorColor="primary"
+        value={value}
+        onChange={handleChange}
+        centered
+        TabIndicatorProps={{ sx: { display: isNonMobile ? 'block' : 'none' } }}
+        sx={{
+          m: '25px',
+          '& .MuiTabs-flexContainer': {
+            flexWrap: 'wrap'
+          }
+        }}
+      >
+        <Tab label="ALL" value="all"></Tab>
+        <Tab label="NEW ARRIVALS" value="newArrivals"></Tab>
+        <Tab label="BEST SELLERS" value="bestSellers"></Tab>
+        <Tab label="TOP RATED" value="topRated"></Tab>
+      </Tabs>
+      <Box
+        margin="0 auto"
+        display="grid"
+        gridTemplateColumns="repeat(auto-fill, 300px)"
+        justifyContent="space-around"
+        rowGap="20px"
+        columnGap="1.33%"
+      >
+        {' '}
+        {value === 'all' &&
+          items.map(item => (
+            <Item item={item} key={`${item.name}-${item.id}`} />
+          ))}
+        {value === 'newArrivals' &&
+          newArrivalsItems.map(item => (
+            <Item item={item} key={`${item.name}-${item.id}`} />
+          ))}
+        {value === 'bestSellers' &&
+          bestSellersItems.map(item => (
+            <Item item={item} key={`${item.name}-${item.id}`} />
+          ))}
+        {value === 'topRated' &&
+          topRatedItems.map(item => (
+            <Item item={item} key={`${item.name}-${item.id}`} />
+          ))}
+      </Box>
+    </Box>
+  );
 };
 
 export default ShoppingList;
