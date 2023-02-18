c8e1c7391656f51189b04176b7a53700fc7354c3
OliGarc333 <ngarcia333@gmail.com>
2023-02-18 14:48:58 +0100

    Preparing for deployment

--- client/src/scenes/checkout/Confirmation.jsx
+++ client/src/scenes/checkout/Confirmation.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
 
 const Confirmation = () => {
-  return <div>Confirmation</div>;
+  return (
+    <div class="card">
+      <div>
+        <i class="checkmark">✓</i>
+      </div>
+      <h1>Success</h1>
+      <p>
+        We received your purchase request;
+        <br /> we'll be in touch shortly!
+      </p>
+    </div>
+  );
 };
 
 export default Confirmation;
